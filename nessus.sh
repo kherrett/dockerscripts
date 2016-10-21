7f8050ab16e289bf7fd7aba6c54489ae22749843
kherrett <kherrett+github@gmail.com>
2016-10-21 13:11:32 -0400

    Update

--- nessus.sh
+++ nessus.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#In case the scanner name isn't set, we'll make sure to choose a random one to avoid duplicates
 random=`echo $((1 + RANDOM % 1000))`
+
+#Remove UUIDs to avoid conflict
 rm -f /etc/tenable_tag
 rm -f /opt/nessus_agent/var/nessus/uuid
 
@@ -22,14 +25,14 @@ if [ ! "$scanner_name" ]; then
         scanner_name="Acme Corp Scanner $random"
 fi
 
-
+#This is for debugging, if you run the container interactively it will spit out the defined variables
 echo "Host is set to $host"
 echo "Port is set to $port"
 echo "Key is set to $key"
 echo "Agent name is set to $scanner_name"
 
+#Link the scanner
 /opt/nessus_agent/sbin/nessuscli managed link --host="$host" --key="$key" --port="$port" --name="""$scanner_name"""
-
-#/opt/nessus/sbin/nessuscli managed link --host=cloud.tenable.com --key=00f0c52cf4d0cda7497932061ff7babd0b1529405bbcaca42f5bc65f64dae1775b --port=443 --name="Acme Corp Internal $random"
+#Start the nessus service
 /opt/nessus/sbin/nessus-service
 
